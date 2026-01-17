6875e8fe75839fd3fa394ab911c326c2c0a5a3b9
Ashesh Mishra <asheshmishra@apples-MacBook-Pro.local>
2026-01-17 20:54:22 +0530

    Update file

--- example/main.go
+++ example/main.go
@@ -1,7 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
-	println("hello world")
-	println("upadte")
+func main() {
+	//TODO -  create vars
+
+	//TODO -  create Structure
+
+	//TODO Implement loop to print struct
+
+	//TO
+	// ============ BASIC SWITCH ============
+	fmt.Println("--- Basic Switch ---")
+	day := "Friday"
+	switch day {
+	case "Monday":
+		fmt.Println("Start of week 📅")
+	case "Friday":
+		fmt.Println("Almost weekend 🎉")
+	case "Saturday", "Sunday":
+		fmt.Println("Weekend! 🏖️")
+	default:
+		fmt.Println("Midweek day")
+	}
 }
