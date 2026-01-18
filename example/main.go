0e195f2ff1ba5cbb58af6a2efae7f540c132d830
Your Name <your-email@example.com>
2026-01-18 11:04:18 +0530

    go lang

--- example/main.go
+++ example/main.go
@@ -4,23 +4,44 @@ import "fmt"
 
 func main() {
 	//TODO -  create vars
+//  {
+//     var age int = 20
+//     var name string = "Gulshan"
+
+//     fmt.Println(age)
+//     fmt.Println(name)
+// }
+
+
+ {
+    city := "Delhi"
+    marks := 85
+
+    fmt.Println(city)
+    fmt.Println(marks)
+}
+
+
+     
+
 
 	//TODO -  create Structure
+
 
 	//TODO Implement loop to print struct
 
 	//TO
 	// ============ BASIC SWITCH ============
-	fmt.Println("--- Basic Switch ---")
-	day := "Friday"
-	switch day {
-	case "Monday":
-		fmt.Println("Start of week 📅")
-	case "Friday":
-		fmt.Println("Almost weekend 🎉")
-	case "Saturday", "Sunday":
-		fmt.Println("Weekend! 🏖️")
-	default:
-		fmt.Println("Midweek day")
-	}
+// 	fmt.Println("--- Basic Switch ---")
+// 	day := "Monday"
+// 	switch day {
+// 	case "Monday":
+// 		fmt.Println("Start of week 📅")
+// 	case "Friday":
+// 		fmt.Println("Almost weekend 🎉")
+// 	case "Saturday", "Sunday":
+// 		fmt.Println("Weekend! 🏖️")
+// 	default:
+// 		fmt.Println("Midweek day")
+// 	}
 }
