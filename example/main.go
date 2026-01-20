b3b73581db4aaa7977802a0ef38ae2fb84eb84fe
Your Name <your-email@example.com>
2026-01-20 20:50:31 +0530

    update 1

--- example/main.go
+++ example/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
-	"example/employee"
+	//"example/employee"
+	"example/student"
 )
 
 func main() {
 
-	var emp []employee.Employee
-	emp = employee.CreateEmployeeData()
-	employee.Display(emp)
+	 stud := []student.Student{
+		{Name: "Anjali", Salary: 52000, Age: 20, StudID: "BTECH05"},
+		{Name: "Rahul", Salary: 60000, Age: 23, StudID: "BTECH06"},
+		{Name: "Neha", Salary: 58000, Age: 22, StudID: "BTECH07"},
+	}
+
+	// var emp []employee.Employee
+	// emp = employee.CreateEmployeeData()
+	// employee.Display(emp)
+
+	//  var stud []student.Student
+	// emp = employee.CreateEmployeeData()
+	 student.Display(stud)
+  
+
+
 
 }
